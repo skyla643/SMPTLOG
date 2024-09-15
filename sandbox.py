ff3c3bf6aaaf9d9f52589aa16289569c775f1222
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2024-09-14 22:05:06 -0400

    now!

--- sandbox.py
+++ sandbox.py
@@ -1,19 +1,20 @@
 import asyncio
-from aiosmtpd import SMTPServer, SMTPSession
+from aiosmtpd.controller import Controller
+from aiosmtpd.handlers import Message
 
-class CustomSMTPSession(SMTPSession):
-    async def handle_DATA(self, data):
-        print('Incoming message from:', self.peer)
-        print('From:', self.mail_from)
-        print('To:', self.mail_to)
+class CustomMessageHandler(Message):
+    async def handle_message(self, message):
+        print('Incoming message from:', message.envelope.mail_from)
+        print('To:', message.envelope.rcpt_tos)
         print('Message:')
-        print(data)
+        print(message.content)
         return '250 OK'
 
 if __name__ == "__main__":
-    server = SMTPServer(('localhost', 1025), CustomSMTPSession)
+    handler = CustomMessageHandler()
+    controller = Controller(handler, hostname='localhost', port=1025)
     print("SMTP server running on localhost:1025")
     try:
-        asyncio.run(server.start())
+        controller.start()
     except KeyboardInterrupt:
         pass
