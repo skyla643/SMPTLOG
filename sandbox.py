c8505e13fd32c9648727a1c714029ac9146b5109
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2024-09-14 21:59:06 -0400

    updates?

--- sandbox.py
+++ sandbox.py
@@ -1,8 +1,7 @@
-# smtp_test_server.py
-import smtpd
-import asyncore
+import smtplib
+import asyncio
 
-class CustomSMTPServer(smtpd.SMTPServer):
+class CustomSMTPServer(smtplib.SMTPServer):
     def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
         print('Incoming message from:', peer)
         print('From:', mailfrom)
@@ -15,6 +14,6 @@ if __name__ == "__main__":
     server = CustomSMTPServer(('localhost', 1025), None)
     print("SMTP server running on localhost:1025")
     try:
-        asyncore.loop()
+        asyncio.run(server.serve_forever())
     except KeyboardInterrupt:
         pass
